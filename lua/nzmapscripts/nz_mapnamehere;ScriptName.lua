eeca175fc534ed733b5bf50acbebd534d20391f7
Logan Christianson <lobsterlogan43@yahoo.com>
2022-08-05 13:20:39 -0500

    Script skeleton

---
+++ lua/nzmapscripts/nz_mapnamehere;ScriptName.lua
@@ -0,0 +1,18 @@
+local mapscript = {}
+
+function mapscript.RoundInit()
+    
+end
+
+-- This one will be called at the start of each round
+function mapscript.RoundStart()
+
+end
+
+-- Will be called after each round
+function mapscript.RoundEnd()
+
+end
+
+-- Always return the mapscript table. This gives it on to the gamemode so it can use it.
+return mapscript
